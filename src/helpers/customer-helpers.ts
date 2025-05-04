3b15ef52a3c7aef0395e634df8b9e2481ef10ecf
tohainam <tohainam.dev@gmail.com>
2025-05-05 00:08:29 +0700

    update time query

--- src/helpers/customer-helpers.ts
+++ src/helpers/customer-helpers.ts
@@ -1,38 +1,48 @@
 import "server-only";
 
 import dayjs from "dayjs";
-// import { revalidateTag } from "next/cache";
-import utc from "dayjs/plugin/utc";
 
-dayjs.extend(utc)
+export const getCustomers = async (
+  accessToken: string,
+  searchText?: string
+) => {
+  console.log("getCustomers", accessToken);
 
-export const getCustomers = async (accessToken: string, searchText?: string) => {
-  console.log('getCustomers', accessToken);
+  const headers = {
+    Retailer: process.env.RETAILER_NAME || "",
+    Authorization: `Bearer ${accessToken}`,
+  };
 
-    const headers = {
-        Retailer: process.env.RETAILER_NAME || '',
-        Authorization: `Bearer ${accessToken}`,
-    };
+  const startTime0 = dayjs().startOf("date").toISOString();
 
-    const startTime = dayjs().utcOffset(0).startOf('date').toISOString();
+  console.log("startTime0", startTime0);
 
-    const response = await fetch(process.env.CUSTOMER_ENDPOINT ? `${process.env.CUSTOMER_ENDPOINT}?lastModifiedFrom=${startTime}&orderBy=modifiedDate&orderDirection=Desc&pageSize=80&includeTotal=true${searchText ? '&name=' + searchText : ''}` : '', {
-        method: "GET",
-        headers: headers,
-    })
-
-    if(response.status === 401) {
-        console.log('Access token expired, revalidating...');
-        // revalidateTag('fetchaccesstoken');
-        throw new Error('Failed to fetch customers because access token expired');
+  const response = await fetch(
+    process.env.CUSTOMER_ENDPOINT
+      ? `${
+          process.env.CUSTOMER_ENDPOINT
+        }?lastModifiedFrom=${startTime0}&orderBy=modifiedDate&orderDirection=Desc&pageSize=80&includeTotal=true${
+          searchText ? "&name=" + searchText : ""
+        }`
+      : "",
+    {
+      method: "GET",
+      headers: headers,
     }
+  );
 
-    if (!response.ok) {
-        // revalidateTag('fetchaccesstoken');
-        throw new Error('Failed to fetch customers');
-    }
+  if (response.status === 401) {
+    console.log("Access token expired, revalidating...");
+    // revalidateTag('fetchaccesstoken');
+    throw new Error("Failed to fetch customers because access token expired");
+  }
 
-    const data = await response.json();
+  if (!response.ok) {
+    // revalidateTag('fetchaccesstoken');
+    throw new Error("Failed to fetch customers");
+  }
 
-    return data;
-}
+  const data = await response.json();
+
+  return data;
+};
